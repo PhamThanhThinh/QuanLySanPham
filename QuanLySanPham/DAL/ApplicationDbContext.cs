d4cadd00d406e5413fefe0948f4424b923ecad54
thinh <thinhqn999@gmail.com>
2025-01-20 21:34:04 +0700

    Add project files.

---
+++ QuanLySanPham/DAL/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLySanPham.Models;
+
+namespace QuanLySanPham.DAL
+{
+  public class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Product> Products { get; set; }
+  }
+}
+
---
+++ QuanLySanPham/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuanLySanPham.Models
+{
+  public class Product
+  {
+    // mã định danh
+    public int Id { get; set; }
+    [Required]
+    //[DisplayName("Tên sản phẩm")]
+    [DisplayName("Ten san pham")]
+    public string ProductName { get; set; }
+    [Required]
+    public double Price { get; set; }
+    [Required]
+    public int Quantity { get; set; }
+  }
+}
