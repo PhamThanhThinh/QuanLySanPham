fe5bb70e5d43ba5c356ca4e2f8cd71fd2b134e8a
thinh <thinhqn999@gmail.com>
2025-02-01 23:25:35 +0700

    final quản lý sản phẩm: thêm xóa sửa

--- QuanLySanPham/Controllers/ProductController.cs
+++ QuanLySanPham/Controllers/ProductController.cs
@@ -15,8 +15,16 @@ namespace QuanLySanPham.Controllers
 
     public async Task<IActionResult> Index()
     {
-      var products = await _unitOfWork.ProductRepository.GetProducts();
-      return View(products);
+      try
+      {
+        var products = await _unitOfWork.ProductRepository.GetProducts();
+        return View(products);
+      }
+      catch (Exception ex)
+      {
+        TempData["errorMessage"] = ex.Message;
+        return View();
+      }
     }
 
     public IActionResult Create()
@@ -29,44 +37,144 @@ namespace QuanLySanPham.Controllers
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Create([Bind("Id, ProductName, Price, Quantity")] Product product)
     {
-      if (ModelState.IsValid)
+      try
       {
-        await _unitOfWork.ProductRepository.Add(product);
-        await _unitOfWork.SaveChangesAsync();
-        return RedirectToAction("Index");
+        if (ModelState.IsValid)
+        {
+          await _unitOfWork.ProductRepository.Add(product);
+          await _unitOfWork.SaveChangesAsync();
+          TempData["successMessage"] = "Sản phẩm đã thêm thành công";
+          return RedirectToAction("Index");
+        }
+        TempData["errorMessage"] = "Sản phẩm không hợp lệ";
+
+        return View(product);
       }
-      return View(product);
+      catch (Exception ex)
+      {
+        TempData["errorMessage"] = ex.Message;
+        return View(product);
+      }
     }
 
     public async Task<IActionResult> Edit(int id)
     {
-      if (id == 0)
+      try
       {
+        if (id == 0)
+        {
+          return View();
+        }
+        var product = await _unitOfWork.ProductRepository.GetProductById(id);
+        if (product == null)
+        {
+          return View();
+        }
+        return View(product);
+      }
+      catch (Exception ex)
+      {
+        TempData["errorMessage"] = ex.Message;
         return View();
       }
-      var product = await _unitOfWork.ProductRepository.GetProductById(id);
-      if (product == null)
-      {
-        return View();
-      }
-      return View(product);
     }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Edit(int id, [Bind("Id, ProductName, Price, Quantity")] Product product)
     {
-      if (id != product.Id)
+      try
       {
+        if (id != product.Id)
+        {
+          return View();
+        }
+        if (ModelState.IsValid)
+        {
+          await _unitOfWork.ProductRepository.Update(product);
+          await _unitOfWork.SaveChangesAsync();
+          TempData["successMessage"] = "Sản phẩm đã sửa thành công";
+          return RedirectToAction("Index");
+        }
+        return View(product);
+      }
+      catch (Exception ex)
+      {
+        TempData["errorMessage"] = ex.Message;
+        return View(product);
+      }
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+      try
+      {
+        if (id == 0)
+        {
+          return View();
+        }
+        var product = await _unitOfWork.ProductRepository.GetProductById(id);
+        if (product == null)
+        {
+          return View();
+        }
+        return View(product);
+      }
+      catch (Exception ex)
+      {
+        TempData["errorMessage"] = ex.Message;
         return View();
       }
-      if (ModelState.IsValid)
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+      try
       {
-        await _unitOfWork.ProductRepository.Update(product);
-        await _unitOfWork.SaveChangesAsync();
-        return RedirectToAction("Index");
+        if (id == 0)
+        {
+          return View();
+        }
+        var product = await _unitOfWork.ProductRepository.GetProductById(id);
+        if (product == null)
+        {
+          return View();
+        }
+        return View(product);
       }
-      return View(product);
+      catch (Exception ex)
+      {
+        TempData["errorMessage"] = ex.Message;
+        return View();
+      }
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id, [Bind("Id, ProductName, Price, Quantity")] Product product)
+    {
+      try
+      {
+        if (id != product.Id)
+        {
+          return View();
+        }
+        if (ModelState.IsValid)
+        {
+          await _unitOfWork.ProductRepository.Delete(id);
+          await _unitOfWork.SaveChangesAsync();
+
+          TempData["successMessage"] = "Sản phẩm đã xóa thành công";
+
+          return RedirectToAction("Index");
+        }
+        return View(product);
+      }
+      catch (Exception ex)
+      {
+        TempData["errorMessage"] = ex.Message;
+        return View(product);
+      }
     }
   }
 }
